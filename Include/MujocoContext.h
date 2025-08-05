acb02292e3632d594caf14e765683f1fd421c88a
DaniAffCH <danieleaffinita2000@gmail.com>
2025-08-05 11:14:00 +0200

    moved mujoco context logic

--- Include/MujocoContext.h
+++ Include/MujocoContext.h
@@ -2,10 +2,6 @@
 
 #include <mujoco/mujoco.h>
 #include <string>
-#include <stdexcept>
-#include <memory>
-#include <functional>
-#include <filesystem>
 namespace spqr {
 
 struct MujocoContext {
@@ -15,61 +11,14 @@ struct MujocoContext {
     mjvOption opt{};
     mjvScene scene{};
 
-    MujocoContext(const std::string& xmlString) {
-        char error[1024] = {0};
-        std::filesystem::current_path(PROJECT_ROOT);
-
-        std::unique_ptr<mjSpec, std::function<void(mjSpec*)> > spec(
-            mj_parseXMLString(xmlString.c_str(), nullptr, error, sizeof(error)),
-            [](mjSpec* s) {
-                mj_deleteSpec(s);
-            }
-        );
-
-        if (!spec) {
-            throw std::runtime_error(std::string("Failed to parse the generated XML: ")+error);
-        }            
-        
-        model = mj_compile(spec.get(), nullptr);
-        if (!model) {
-            throw std::runtime_error(std::string("Failed to compile mujoco specs: ")+mjs_getError(spec.get()));
-        }
-
-        data = mj_makeData(model);
-
-        mjv_defaultOption(&opt);
-        mjv_defaultCamera(&cam);
-        mjv_makeScene(model, &scene, 10000);
-    }
-
-    ~MujocoContext() {
-        if (data) mj_deleteData(data);
-        if (model) mj_deleteModel(model);
-        mjv_freeScene(&scene);
-    }
+    MujocoContext(const std::string& xmlString);
+    ~MujocoContext();
 
     // Copying could potentially lead to freeing the model or data twice.
     // Deleting the copy constructors prevents this.
     MujocoContext(const MujocoContext&) = delete;
     MujocoContext& operator=(const MujocoContext&) = delete;
 
-    MujocoContext& operator=(MujocoContext&& other) noexcept {
-        if (this != &other) {
-            // free existing resources
-            if (data) mj_deleteData(data);
-            if (model) mj_deleteModel(model);
-            mjv_freeScene(&scene);
-
-            model = other.model;
-            data = other.data;
-            cam = other.cam;
-            opt = other.opt;
-            scene = other.scene;
-
-            other.model = nullptr;
-            other.data = nullptr;
-        }
-        return *this;
-    }
+    MujocoContext& operator=(MujocoContext&& other) noexcept;
 };
 }
---
+++ Src/MujocoContext.cpp
@@ -0,0 +1,58 @@
+#include "MujocoContext.h"
+#include <stdexcept>
+#include <memory>
+#include <functional>
+#include <filesystem>
+
+namespace spqr {
+    MujocoContext::MujocoContext(const std::string& xmlString){
+        char error[1024] = {0};
+        std::filesystem::current_path(PROJECT_ROOT);
+
+        std::unique_ptr<mjSpec, std::function<void(mjSpec*)> > spec(
+            mj_parseXMLString(xmlString.c_str(), nullptr, error, sizeof(error)),
+            [](mjSpec* s) {
+                mj_deleteSpec(s);
+            }
+        );
+
+        if (!spec) {
+            throw std::runtime_error(std::string("Failed to parse the generated XML. ")+error);
+        }            
+        
+        model = mj_compile(spec.get(), nullptr);
+        if (!model) {
+            throw std::runtime_error(std::string("Failed to compile mujoco specs. ")+mjs_getError(spec.get()));
+        }
+
+        data = mj_makeData(model);
+
+        mjv_defaultOption(&opt);
+        mjv_defaultCamera(&cam);
+        mjv_makeScene(model, &scene, 10000);
+    }
+
+    MujocoContext::~MujocoContext(){
+        if (data) mj_deleteData(data);
+        if (model) mj_deleteModel(model);
+        mjv_freeScene(&scene);
+    }
+
+    MujocoContext& MujocoContext::operator=(MujocoContext&& other) noexcept {
+        if (this != &other) {
+            if (data) mj_deleteData(data);
+            if (model) mj_deleteModel(model);
+            mjv_freeScene(&scene);
+
+            model = other.model;
+            data = other.data;
+            cam = other.cam;
+            opt = other.opt;
+            scene = other.scene;
+
+            other.model = nullptr;
+            other.data = nullptr;
+        }
+        return *this;
+    }
+}

7b10bc780e34bdfc712016d28db216d1ad82a775
Valerio Spagnoli <spagnoli.valerio.00@gmail.com>
2025-08-28 18:33:46 +0200

    Merge Drag and Drop into camera simulation

---
+++ Include/Robot.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <mujoco/mujoco.h>
+
+#include "SceneParser.h"
+
+namespace spqr {
+
+struct Robot {
+    int index = -1;            // Index in the list of robots
+    RobotInfo info;            // Information from sceneParser
+    int rootBodyId;            // body if of the root (for mjData)
+    std::vector<int> bodyIds;  // all body ids belonging to this robot
+    std::vector<int> geomIds;  // all geom ids belonging to this robot
+
+    const mjtNum* worldPos(const mjData* d) const {
+        return d->xpos + 3 * rootBodyId;
+    }
+
+    bool ownsGeom(int geomId) const {
+        return std::find(geomIds.begin(), geomIds.end(), geomId) != geomIds.end();
+    }
+
+    bool ownsBody(int bodyId) const {
+        return std::find(bodyIds.begin(), bodyIds.end(), bodyId) != bodyIds.end();
+    }
+
+    bool ownsBody(const std::string& name) const {
+        std::string prefix = info.name + "_";
+        return name.compare(0, prefix.size(), prefix) == 0;
+    }
+};
+
+}  // namespace spqr
---
+++ Include/RobotManager.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <mujoco/mujoco.h>
+
+#include <vector>
+
+#include "Robot.h"
+#include "SceneParser.h"
+
+namespace spqr {
+
+class RobotManager {
+   public:
+    void build(const mjModel* m, const SceneInfo& sceneSpec);
+    int robotIndexByBody(int bodyId) const;
+    int robotIndexByGeom(int geomId, const mjModel* m) const;
+    int rootBodyIndex(int bodyId) const;
+    const Robot* get(int idx) const;
+    void highlightRobot(int bodyId, mjvScene* scene) const;
+
+   private:
+    std::vector<Robot> robots;     // size: from robotspecs
+    std::vector<int> bodyToRobot;  // size: mjModel->nbody
+};
+
+}  // namespace spqr
